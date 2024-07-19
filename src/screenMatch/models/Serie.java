dbfa32cd0bc9b740e171bf01526ae5f1c8dc515c
Rafael <rafaasigoli1@gmail.com>
2024-07-18 22:55:11 -0300

    ScreenMatch v1.1

---
+++ src/screenMatch/models/Serie.java
@@ -0,0 +1,50 @@
+package screenMatch.models;
+
+public class Serie extends Title {
+    private int seasons;
+    private int episodesPerSeasons;
+    private boolean active;
+    private int minutesPerEpisodes;
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public int getEpisodesPerSeasons() {
+        return episodesPerSeasons;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public int getMinutesPerEpisodes() {
+        return minutesPerEpisodes;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public void setEpisodesPerSeasons(int episodesPerSeasons) {
+        this.episodesPerSeasons = episodesPerSeasons;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setMinutesPerEpisodes(int minutesPerEpisodes) {
+        this.minutesPerEpisodes = minutesPerEpisodes;
+    }
+
+    @Override
+    public int getMinutesDuration() {
+        return seasons * episodesPerSeasons * minutesPerEpisodes;
+    }
+
+    @Override
+    public String toString(){
+        return "Serie: " + this.name + "( " + this.releaseYear + " )";
+    }
+}
